b91db27e32aa7460b65b9f7a4c7a77aa207cf94e
jacobgladish <jacobgladish@yahoo.com>
2015-01-20 18:02:55 -0500

    Added missing files

---
+++ Spotify/AbstractAsyncLoadable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Spotify
+{
+    public abstract class AbstractAsyncLoadable : DomainObject
+    {
+        #region Events
+        public abstract event EventHandler Loaded;
+        #endregion
+
+        internal AbstractAsyncLoadable(IntPtr handle, AddRef addRef, DelRef delRef, bool preIncremented)
+            : base(handle, addRef, delRef, preIncremented)
+        {
+        }
+
+        public abstract bool IsLoaded
+        {
+            get;
+        }
+
+        internal void RegisterLoadedHandler(EventHandler h)
+        {
+            Loaded += h;
+        }
+    }
+}
---
+++ Spotify/PlaylistImageChangedEventArgs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Spotify
+{
+    using Spotify.Internal;
+
+    public class PlaylistImageChangedEventArgs : EventArgs
+    {
+        internal PlaylistImageChangedEventArgs(IntPtr imageId)
+        {
+            ImageId = imageId;
+        }
+
+        #region Async Methods
+        public Task<Image> LoadImageAsync(Session session, AsyncCallback userCallback, object state)
+        {
+            return Task.Factory.FromAsync<Session, Image>(BeginLoadImage, EndLoadImage, session, state);
+        }
+
+        public IAsyncResult BeginLoadImage(Session session, AsyncCallback userCallback, object state)
+        {
+            return ImageLoader.Begin((p, s) => { return p; }, ImageId, session, ImageSize.Normal, userCallback, state);
+        }
+
+        public Image EndLoadImage(IAsyncResult result)
+        {
+            return ImageLoader.End(result);
+        }
+        #endregion
+
+        private readonly IntPtr ImageId;
+    }
+}
